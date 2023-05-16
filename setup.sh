70eed7f538375b5ba73f2520050e1d0339e45f78
Will Dean <william.dean@free-now.com>
2023-05-16 09:26:26 +0200

    just cd

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
 
 ln -s $PWD/vim/.vimrc $HOME/.vimrc
 
-for file in $PWD/oh-my-zsh/**/*.zsh
-do ln -s $file $ZSH_CUSTOM
-done 
+cd oh-my-zsh
+for file in *.zsh
+do ln -s $file $ZSH_CUSTOM/$file
+done
+cd ..
