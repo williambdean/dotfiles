d0e891ae4cf8f1d9f93c82c17d2ed459af0837e7
Will Dean <wd60622@gmail.com>
2024-06-04 05:19:50 +0200

    add wezterm config

--- vim/init.lua
+++ vim/init.lua
@@ -297,6 +297,7 @@ require("lazy").setup({
         dependencies = { 'nvim-lua/plenary.nvim' }, 
         keys = {
             { "<leader>ff", function() require('telescope.builtin').find_files() end, },
+            { "<leader>fd", function() require('telescope.builtin').find_files({ hidden = true }) end, },
             { "<leader>fg", function() require('telescope.builtin').live_grep() end, },
             { "<leader>fb", function() require('telescope.builtin').buffers() end, },
             { "<leader>fh", function() require('telescope.builtin').help_tags() end, },
