a55a4a8f9fc2837b53c3b5a2efb3140fe59aa6db
Will Dean <wd60622@gmail.com>
2024-07-10 19:39:19 +0200

    search python environment packages

--- vim/lua/plugins/telescope.lua
+++ vim/lua/plugins/telescope.lua
@@ -1,9 +1,33 @@
+local find_site_packages = function()
+	local site_packages = vim.trim(vim.fn.system("python -c 'import site; print(site.getsitepackages()[0])'"))
+
+	return site_packages
+end
+
 return {
 	{ "nvim-telescope/telescope-symbols.nvim" },
 	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = {
+			{
+				"<leader>pf",
+				function()
+					require("telescope.builtin").find_files({
+						cwd = find_site_packages(),
+						glob_pattern = "*.py",
+					})
+				end,
+			},
+			{
+				"<leader>pg",
+				function()
+					require("telescope.builtin").live_grep({
+						cwd = find_site_packages(),
+						glob_pattern = "*.py",
+					})
+				end,
+			},
 			{
 				"<leader>fc",
 				function()
