5e43687c9664229a637f015878e75ab085f01ee6
Will Dean <wd60622@gmail.com>
2024-08-14 14:19:30 +0200

    try out on zsh file

--- oh-my-zsh/alias.zsh
+++ oh-my-zsh/alias.zsh
@@ -24,5 +24,8 @@ alias gls='git ls-tree --full-tree --name-only -r HEAD'
 alias gg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
 alias gdelete="git branch --merged | grep -v \* | xargs git branch -D "
 
+# GitHub CLI
+alias ic="gh issue create -e"
+
 # Docker 
 alias enter-docker="docker run --rm -it --entrypoint bash"
