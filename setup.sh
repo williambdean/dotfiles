84cb16c5d6830d86a3106d3e33e17359465a832b
Will Dean <wd60622@gmail.com>
2024-08-14 14:19:13 +0200

    try out the formatting

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $PWD/vim/.vimrc $HOME/.vimrc
 
 cd oh-my-zsh
-for file in *.zsh
-do ln -s $file $ZSH_CUSTOM/$file
+for file in *.zsh; do
+	ln -s $file $ZSH_CUSTOM/$file
 done
 cd ..
