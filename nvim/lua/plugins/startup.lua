a7001e99150e4849bcda264196fabf23e62e64a7
Will Dean <wd60622@gmail.com>
2024-08-18 19:18:23 +0200

    placeholder for dashboard

--- nvim/lua/plugins/startup.lua
+++ nvim/lua/plugins/startup.lua
@@ -1,38 +1,39 @@
 return {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    config = function()
-        local db = require("dashboard")
-        db.setup({
-            theme = "hyper",
-            config = {
-                week_header = {
-                    enable = true,
-                },
-                shortcut = {
-                    {
-                        desc = "󰊳 Update",
-                        group = "@property",
-                        action = "Lazy update",
-                        key = "u",
-                    },
-                    {
-                        icon = " ",
-                        icon_hl = "@variable",
-                        desc = "Files",
-                        group = "Label",
-                        action = "Telescope find_files",
-                        key = "f",
-                    },
-                    {
-                        desc = " dotfiles",
-                        group = "Number",
-                        action = "Z dotfiles",
-                        key = "d",
-                    },
-                },
-            },
-        })
-    end,
-    dependencies = { { "nvim-tree/nvim-web-devicons" } },
+    --     {    "nvimdev/dashboard-nvim",
+    --     event = "VimEnter",
+    --     config = function()
+    --         local db = require("dashboard")
+    --         db.setup({
+    --             theme = "hyper",
+    --             config = {
+    --                 week_header = {
+    --                     enable = true,
+    --                 },
+    --                 shortcut = {
+    --                     {
+    --                         desc = "󰊳 Update",
+    --                         group = "@property",
+    --                         action = "Lazy update",
+    --                         key = "u",
+    --                     },
+    --                     {
+    --                         icon = " ",
+    --                         icon_hl = "@variable",
+    --                         desc = "Files",
+    --                         group = "Label",
+    --                         action = "Telescope find_files",
+    --                         key = "f",
+    --                     },
+    --                     {
+    --                         desc = " dotfiles",
+    --                         group = "Number",
+    --                         action = "Z dotfiles",
+    --                         key = "d",
+    --                     },
+    --                 },
+    --             },
+    --         })
+    --     end,
+    --     dependencies = { { "nvim-tree/nvim-web-devicons" } },
+    -- }
 }
