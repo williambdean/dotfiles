a19dd3dc7503cc9709073a428adaad66fc59a89a
Will Dean <wd60622@gmail.com>
2024-09-24 10:08:52 -0400

    use the clip board again

--- nvim/init.lua
+++ nvim/init.lua
@@ -143,20 +143,20 @@ vim.opt.backspace:append({ "indent", "eol", "start" })
 -- vim.api.nvim_set_keymap("v", "<leader>y", '"*y', { noremap = true })
 --
 
--- vim.opt.clipboard = 'unnamedplus'
---
--- vim.g.clipboard = {
---     name = 'WslClipboard',
---     copy = {
---         ['+'] = 'clip.exe',
---         ['*'] = 'clip.exe',
---     },
---     paste = {
---         ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---         ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---     },
---     cache_enabled = 0,
--- }
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+        ["+"] = "clip.exe",
+        ["*"] = "clip.exe",
+    },
+    paste = {
+        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+}
 
 -- vim.g.clipboard = {
 --     name = "WslClipboard",
