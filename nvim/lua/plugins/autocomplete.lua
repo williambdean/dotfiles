c4184c2f44e2a72ed527d198935dd1cbb644cbcf
Will Dean <wd60622@gmail.com>
2024-09-26 08:02:25 -0400

    add nvim-cmp

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -26,6 +26,37 @@ local function quick_chat(with_buffer)
 end
 
 return {
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+
+                mapping = {
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.close(),
+                    ["<CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    }),
+                },
+
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "vsnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+            })
+        end,
+    },
     { "github/copilot.vim" },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
