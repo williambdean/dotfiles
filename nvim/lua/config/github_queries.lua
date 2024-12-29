d2f8a3d4a0213136c79df2c5526dd166edc7d561
Will Dean <wd60622@gmail.com>
2024-12-29 12:22:16 +0100

    rename files

--- nvim/lua/config/github_queries.lua
+++ nvim/lua/config/github_queries.lua
@@ -1,6 +1,7 @@
+---Create a command to execute a GraphQL query from a file
 local gh = require("octo.gh")
 
-local execute_query = function(query)
+local sync_github_cli_query = function(query)
   local output = gh.run({
     args = { "api", "graphql", "-f", "query=" .. query },
     mode = "sync",
@@ -8,23 +9,20 @@ local execute_query = function(query)
   return vim.fn.json_decode(output)
 end
 
-local function execute_file_query(start_line, end_line)
+local function execute_query(start_line, end_line)
   start_line = start_line or 0
   end_line = end_line or -1
   local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
   local query = table.concat(lines, "\n")
-  local resp = execute_query(query)
+  local resp = sync_github_cli_query(query)
   vim.print(resp)
 end
 
 vim.api.nvim_create_user_command("Query", function(args)
   local start_line, end_line
   if args.range ~= 0 then
-    start_line = args.line1
+    start_line = args.line1 - 1
     end_line = args.line2
-  else
-    start_line = 0
-    end_line = -1
   end
-  execute_file_query(start_line, end_line)
+  execute_query(start_line, end_line)
 end, { nargs = "*", range = true })
