962cf6707454f7508731268cf2290320f7b833fa
Will Dean <wd60622@gmail.com>
2024-12-29 14:36:55 +0100

    expose the timeout

--- nvim/lua/config/github_queries.lua
+++ nvim/lua/config/github_queries.lua
@@ -1,7 +1,19 @@
 ---Create a command to execute a GraphQL query from a file
 local gh = require("octo.gh")
+local config = require("octo.config")
 
-local sync_github_cli_query = function(query)
+---@class Query
+---@field query string
+---@field timeout number|nil The timeout in seconds
+
+---@param opts Query
+---@return table
+local sync_github_cli_query = function(opts)
+  local original_timeout = config.values.timeout
+  if opts.timeout then
+    config.values.timeout = opts.timeout * 1000
+  end
+
   local output = gh.run({
     args = {
       "api",
@@ -9,11 +21,14 @@ local sync_github_cli_query = function(query)
       "--paginate",
       "--slurp",
       "-f",
-      "query=" .. query,
+      "query=" .. opts.query,
     },
     mode = "sync",
   })
   local resp = vim.fn.json_decode(output)
+
+  config.values.timeout = original_timeout
+
   if #resp == 1 then
     return resp[1]
   end
@@ -21,22 +36,54 @@ local sync_github_cli_query = function(query)
   return resp
 end
 
-local function execute_query(start_line, end_line)
+---@param start_line number|nil
+---@param end_line number|nil
+---@param timeout number|nil The timeout in seconds
+local function execute_query(start_line, end_line, timeout)
   start_line = start_line or 0
   end_line = end_line or -1
   local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
   local query = table.concat(lines, "\n")
-  return sync_github_cli_query(query)
+  return sync_github_cli_query({
+    query = query,
+    timeout = timeout,
+  })
 end
 
 local write_to_file = function(file, content)
   local f = io.open(file, "w")
+  if f == nil then
+    vim.notify("Failed to open file " .. file)
+    return
+  end
+
   f:write(content)
   f:close()
 end
 
 local write_json_to_file = function(file, content)
   write_to_file(file, vim.fn.json_encode(content))
+end
+
+---@class QueryArgs
+---@field file string The file to write the response to
+---@field timeoout number The timeout in seconds
+
+---@param args string
+---@return QueryArgs
+local parse_args = function(args)
+  local file = ""
+  local timeout = nil
+  local split = vim.split(args, " ")
+  for _, arg in ipairs(split) do
+    if arg:match(".json$") then
+      file = arg
+    end
+    if arg:match("--timeout") then
+      timeout = tonumber(vim.split(arg, "=")[2])
+    end
+  end
+  return { file = file, timeout = timeout }
 end
 
 vim.api.nvim_create_user_command("Query", function(args)
@@ -46,10 +93,11 @@ vim.api.nvim_create_user_command("Query", function(args)
     end_line = args.line2
   end
 
-  local resp = execute_query(start_line, end_line)
+  local query_args = parse_args(args.args)
+  local resp = execute_query(start_line, end_line, query_args.timeout)
 
-  local file = args.args
-  if file ~= "" and file:match(".json$") then
+  local file = query_args.file
+  if file ~= "" then
     vim.notify("Writing response to " .. file)
     write_json_to_file(file, resp)
   else
