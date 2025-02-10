b910181629acff9419b3bc087630fefa543e071c
Will Dean <wd60622@gmail.com>
2025-02-10 10:17:45 +0100

    check for uv environment

--- nvim/lua/config/python.lua
+++ nvim/lua/config/python.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 M.get_executable = function()
-  return vim.fn.exepath "python"
+  if vim.fn.filereadable "uv.lock" == 1 then
+    return "uv run python"
+  else
+    return vim.fn.exepath "python"
+  end
 end
 
 M.has_ipython = function()
