641aa3ba30a60eda8c1eb9e409cc4381a703aba8
Will Dean <wd60622@gmail.com>
2025-03-26 17:38:07 +0100

    giving blink a try

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -25,53 +25,209 @@ local function quick_chat(with_buffer)
   vim.ui.input({ prompt = prompt }, handle_input)
 end
 
+vim.keymap.set("i", "@", "@<C-x><C-o>", { silent = true, buffer = true })
+vim.keymap.set("i", "#", "#<C-x><C-o>", { silent = true, buffer = true })
+
 return {
   {
-    "hrsh7th/nvim-cmp",
-    lazy = true,
-    event = {
-      "InsertEnter",
-      "CmdlineEnter",
+    "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = { "rafamadriz/friendly-snippets" },
+
+    -- use a release tag to download pre-built binaries
+    version = "*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = "default" },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    config = function()
-      local cmp = require "cmp"
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end,
-        },
-        completion = {
-          keyword_length = 1, -- Set the minimum keyword length to trigger completion
-        },
-        mapping = {
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
-          ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-          },
-        },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "vsnip" },
-          { name = "buffer" },
-          { name = "path" },
-          { name = "copilot" },
-          { name = "graphql" },
-        },
-      }
-      cmp.setup.filetype("copilot-chat", {
-        sources = cmp.config.sources {
-          { name = "copilot" },
-          { name = "buffer" },
-          { name = "path" },
-        },
-      })
-    end,
+    opts_extend = { "sources.default" },
+
+    --- Custom
+
+    cmdline = { enabled = false },
   },
+  -- {
+  --   'echasnovski/mini.completion',
+  --   version = '*',
+  --   opts = {},
+  -- },
+
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   version = false, -- last release is way too old
+  --   event = "InsertEnter",
+  --   dependencies = {
+  --     "hrsh7th/cmp-nvim-lsp",
+  --     "hrsh7th/cmp-buffer",
+  --     "hrsh7th/cmp-path",
+  --   },
+  --   -- Not all LSP servers add brackets when completing a function.
+  --   -- To better deal with this, LazyVim adds a custom option to cmp,
+  --   -- that you can configure. For example:
+  --   --
+  --   -- ```lua
+  --   -- opts = {
+  --   --   auto_brackets = { "python" }
+  --   -- }
+  --   -- ```
+  --   opts = function()
+  --     vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+  --     local cmp = require("cmp")
+  --     local defaults = require("cmp.config.default")()
+  --     local auto_select = true
+  --     return {
+  --       auto_brackets = {}, -- configure any filetype to auto add brackets
+  --       completion = {
+  --         completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+  --       },
+  --       preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+  --       mapping = cmp.mapping.preset.insert({
+  --         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+  --         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  --         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+  --         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+  --         ["<C-Space>"] = cmp.mapping.complete(),
+  --         -- ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+  --         -- ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
+  --         -- ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  --         ["<C-CR>"] = function(fallback)
+  --           cmp.abort()
+  --           fallback()
+  --         end,
+  --         -- ["<tab>"] = function(fallback)
+  --         --   return LazyVim.cmp.map({ "snippet_forward", "ai_accept" }, fallback)()
+  --         -- end,
+  --       }),
+  --       sources = cmp.config.sources({
+  --         { name = "lazydev" },
+  --         { name = "nvim_lsp" },
+  --         { name = "path" },
+  --       }, {
+  --         { name = "buffer" },
+  --       }),
+  --       formatting = {
+  --         format = function(entry, item)
+  --           local icons = LazyVim.config.icons.kinds
+  --           if icons[item.kind] then
+  --             item.kind = icons[item.kind] .. item.kind
+  --           end
+  --
+  --           local widths = {
+  --             abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+  --             menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+  --           }
+  --
+  --           for key, width in pairs(widths) do
+  --             if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+  --               item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+  --             end
+  --           end
+  --
+  --           return item
+  --         end,
+  --       },
+  --       experimental = {
+  --         -- only show ghost text when we show ai completions
+  --         ghost_text = vim.g.ai_cmp and {
+  --           hl_group = "CmpGhostText",
+  --         } or false,
+  --       },
+  --       sorting = defaults.sorting,
+  --     }
+  --   end,
+  --   main = "lazyvim.util.cmp",
+  -- },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   lazy = true,
+  --   event = {
+  --     "InsertEnter",
+  --     "CmdlineEnter",
+  --   },
+  --   config = function()
+  --     local cmp = require "cmp"
+  --     cmp.setup {
+  --       snippet = {
+  --         expand = function(args)
+  --           vim.fn["vsnip#anonymous"](args.body)
+  --         end,
+  --       },
+  --       completion = {
+  --         keyword_length = 1, -- Set the minimum keyword length to trigger completion
+  --       },
+  --       mapping = {
+  --         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+  --         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  --         ["<C-Space>"] = cmp.mapping.complete(),
+  --         ["<C-e>"] = cmp.mapping.close(),
+  --         ["<CR>"] = cmp.mapping.confirm {
+  --           behavior = cmp.ConfirmBehavior.Insert,
+  --           select = true,
+  --         },
+  --       },
+  --       sources = {
+  --         { name = "nvim_lsp" },
+  --         { name = "vsnip" },
+  --         { name = "buffer" },
+  --         { name = "path" },
+  --         { name = "copilot" },
+  --         { name = "graphql" },
+  --       },
+  --     }
+  --     cmp.setup.filetype("copilot-chat", {
+  --       sources = cmp.config.sources {
+  --         { name = "copilot" },
+  --         { name = "buffer" },
+  --         { name = "path" },
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = { "Copilot" },
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup {}
+  --   end,
+  -- },
   {
     "github/copilot.vim",
     config = function()
@@ -115,48 +271,39 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
     dependencies = {
-      { "github/copilot.vim" },
+      { "zbirenbaum/copilot.lua" },
+      -- { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim" },
       { "nvim-telescope/telescope.nvim" },
     },
-    config = function()
+    init = function()
       -- Custom buffer for CopilotChat
       vim.api.nvim_create_autocmd("BufEnter", {
         pattern = "copilot-*",
         callback = function()
-          vim.opt_local.relativenumber = true
-          vim.opt_local.number = true
-
-          -- Get current filetype and set it to markdown if the current filetype is copilot-chat
-          -- local ft = vim.bo.filetype
-          -- if ft == "copilot-chat" then
-          --     vim.bo.filetype = "markdown"
-          -- end
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
         end,
       })
-      require("CopilotChat").setup {
-        debug = false,
-        chat_autocomplete = true,
-        auto_follow_cursor = true,
-        -- model = "claude-3.5-sonnet",
-        show_help = false,
-        context = "buffers",
-        language = "English",
-        prompts = {
-          Explain = "Explain how it works in the English language.",
-          Review = "Review the following code and provide concise suggestions.",
-          Tests = "Write tests for the following code.",
-          Anything = {
-            system_prompt = "Ignore any previous prompts and respond to the following question. It will likely be a technical question but could be anything.",
-            prompt = "This is a free-form prompt. Respond to the following question.",
-          },
-        },
-        build = function()
-          vim.notify "Please update the remote plugins by running :UpdateRemotePlugins, the"
-        end,
-        event = "VeryLazy",
-      }
     end,
+    opts = {
+      debug = false,
+      chat_autocomplete = true,
+      auto_follow_cursor = true,
+      model = "claude-3.7-sonnet",
+      show_help = false,
+      context = "buffers",
+      language = "English",
+      prompts = {
+        Explain = "Explain how it works in the English language.",
+        Review = "Review the following code and provide concise suggestions.",
+        Tests = "Write tests for the following code.",
+      },
+      build = function()
+        vim.notify "Please update the remote plugins by running :UpdateRemotePlugins, the"
+      end,
+      event = "VeryLazy",
+    },
     keys = {
       {
         "<leader>t",
