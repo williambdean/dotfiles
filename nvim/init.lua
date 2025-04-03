5447e8161c373665b12157fd5da9f07aa2397934
Will Dean <wd60622@gmail.com>
2025-04-03 01:12:42 -0400

    updates with new machine

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,15 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
+  }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -115,7 +122,13 @@ require("lazy").setup {
       opts = {},
     },
     { "dstein64/vim-startuptime", cmd = "StartupTime" },
-    { "folke/snacks.nvim" },
+    {
+      "folke/snacks.nvim",
+      ---@type snacks.Config
+      opts = {
+        picker = { enabled = true },
+      },
+    },
     {
       "folke/which-key.nvim",
       event = "VeryLazy",
