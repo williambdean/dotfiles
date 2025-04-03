f61106e1b8abc0170f6ac9e3c395247f7808d9c6
Will Dean <wd60622@gmail.com>
2025-04-03 15:29:41 -0400

    run pre-commit

--- nvim/lua/config/obsidian.lua
+++ nvim/lua/config/obsidian.lua
@@ -1,13 +1,14 @@
 --- Finds the location of obsidian file
 local M = {}
 
-local directory = [[Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/will]]
+local directory =
+  [[Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/will]]
 M.directory = vim.fn.expand("~/" .. directory)
 
 M.directory_exists = vim.fn.isdirectory(M.directory) == 1
 
 M.file_name = function(file_name)
-  local directory = M.directory_exists and M.directory or vim.fn.expand("~/")
+  local directory = M.directory_exists and M.directory or vim.fn.expand "~/"
   return directory .. "/" .. file_name
 end
 
