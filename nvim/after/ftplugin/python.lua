243335af99f7fc5f68b1769a05449b48ac0c5123
Will Dean <wd60622@gmail.com>
2025-06-19 21:22:11 -0400

    conditional <leader>x based on file

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,6 +1,10 @@
 local terminal = require "config.terminal"
 local python = require "config.python"
 
+local is_test_file = function(filename)
+  return filename:match "test_.*%.py"
+end
+
 local opts = {
   noremap = true,
   silent = true,
@@ -9,8 +13,18 @@ local opts = {
 vim.keymap.set("n", "<leader>x", function(args)
   local filename = vim.fn.expand "%:p"
 
+  if is_test_file(filename) then
+    local tests = terminal.get_test_under_cursor()
+    terminal.run_test({
+      args = "--pdb -vvv",
+    }, tests)
+    return
+  end
+
   local command
-  if python.has_ipython() then
+  if python.has_uv() then
+    command = "uv run python -i " .. filename
+  elseif python.has_ipython() then
     command = "ipython -i " .. filename
   else
     command = "python -i " .. filename
