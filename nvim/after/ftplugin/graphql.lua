57c6b7ba704307aa1a821d59e643e10f78774307
Will Dean <wd60622@gmail.com>
2025-10-26 00:25:51 -0400

    add helper for query

--- nvim/after/ftplugin/graphql.lua
+++ nvim/after/ftplugin/graphql.lua
@@ -4,3 +4,10 @@ set.expandtab = true
 set.shiftwidth = 2
 set.softtabstop = 2
 set.tabstop = 2
+
+vim.keymap.set(
+  "n",
+  "<leader>x",
+  "<CMD>Query<CR>",
+  { noremap = true, silent = true, buffer = true }
+)
