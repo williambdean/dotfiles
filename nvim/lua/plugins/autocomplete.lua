ea766a7705da75446bcaa28455400e967722eedc
Will Dean <wd60622@gmail.com>
2025-11-11 13:11:36 -0500

    Alternative setup

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -311,6 +311,84 @@ return {
       })
     end,
   },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   branch = "main",
+  --   cmd = "CopilotChat",
+  --   opts = function()
+  --     local user = vim.env.USER or "User"
+  --     user = user:sub(1, 1):upper() .. user:sub(2)
+  --     return {
+  --       model = "claude-3-5-sonnet",
+  --       auto_insert_mode = true,
+  --       headers = {
+  --         user = "  " .. user .. " ",
+  --         assistant = "  Copilot ",
+  --         tool = "󰊳  Tool ",
+  --       },
+  --       window = {
+  --         width = 0.4,
+  --       },
+  --     }
+  --   end,
+  --   keys = {
+  --     { "<c-s>",     "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
+  --     { "<leader>a", "",     desc = "+ai",        mode = { "n", "x" } },
+  --     {
+  --       "<leader>aa",
+  --       function()
+  --         return require("CopilotChat").toggle()
+  --       end,
+  --       desc = "Toggle (CopilotChat)",
+  --       mode = { "n", "x" },
+  --     },
+  --     {
+  --       "<leader>ax",
+  --       function()
+  --         return require("CopilotChat").reset()
+  --       end,
+  --       desc = "Clear (CopilotChat)",
+  --       mode = { "n", "x" },
+  --     },
+  --     {
+  --       "<leader>aq",
+  --       function()
+  --         vim.ui.input({
+  --           prompt = "Quick Chat: ",
+  --         }, function(input)
+  --           if input ~= "" then
+  --             require("CopilotChat").ask(input)
+  --           end
+  --         end)
+  --       end,
+  --       desc = "Quick Chat (CopilotChat)",
+  --       mode = { "n", "x" },
+  --     },
+  --     {
+  --       "<leader>ap",
+  --       function()
+  --         require("CopilotChat").select_prompt()
+  --       end,
+  --       desc = "Prompt Actions (CopilotChat)",
+  --       mode = { "n", "x" },
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     local chat = require("CopilotChat")
+  --
+  --     vim.notify(vim.inspect(opts))
+  --
+  --     vim.api.nvim_create_autocmd("BufEnter", {
+  --       pattern = "copilot-chat",
+  --       callback = function()
+  --         vim.opt_local.relativenumber = false
+  --         vim.opt_local.number = false
+  --       end,
+  --     })
+  --
+  --     chat.setup(opts)
+  --   end,
+  -- },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
@@ -332,9 +410,9 @@ return {
     end,
     opts = {
       debug = false,
+      model = "claude-haiku-4.5",
       chat_autocomplete = true,
       auto_follow_cursor = true,
-      model = "claude-3.7-sonnet",
       show_help = false,
       context = "buffers",
       language = "English",
