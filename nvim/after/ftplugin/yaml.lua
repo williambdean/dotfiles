464c29635623bde329e0b7c126966a72efaf37e0
Will Dean <wd60622@gmail.com>
2025-11-18 11:27:22 -0500

    migrate the yaml lsp

--- nvim/after/ftplugin/yaml.lua
+++ nvim/after/ftplugin/yaml.lua
@@ -7,16 +7,15 @@ set.shiftwidth = 2
 set.softtabstop = 2
 set.tabstop = 2
 
-require("lspconfig").yamlls.setup {
-  settings = {
-    yaml = {
-      validate = true,
-      schemas = {
-        ["https://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-        ["https://json.schemastore.org/github-action"] = ".github/actions.{yml,yaml}",
-        ["https://json.schemastore.org/conda-forge"] = "environment.{yml,yaml}",
-        ["https://json.schemastore.org/pre-commit-config"] = ".pre-commit-config.{yml,yaml}",
-      },
+vim.lsp.enable "yamlls"
+vim.lsp.config.yamlls.settings = {
+  yaml = {
+    validate = true,
+    schemas = {
+      ["https://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+      ["https://json.schemastore.org/github-action"] = ".github/actions.{yml,yaml}",
+      ["https://json.schemastore.org/conda-forge"] = "environment.{yml,yaml}",
+      ["https://json.schemastore.org/pre-commit-config"] = ".pre-commit-config.{yml,yaml}",
     },
   },
 }
