40dd5c132328870cb1bf8f8b674592bf16d2c5d6
Will Dean <wd60622@gmail.com>
2026-06-12 10:06:07 -0400

    Execute with different code blocks

--- nvim/lua/config/execute.lua
+++ nvim/lua/config/execute.lua
@@ -1,5 +1,3 @@
---- Execute a block of python code
----
 local python = require "config.python"
 local Job = require "plenary.job"
 
@@ -22,38 +20,67 @@ end
 M.async = function(code, callback)
   local executable = python.get_executable()
 
-  Job:new({
-    command = executable,
-    args = { "-c", code },
-    on_exit = vim.schedule_wrap(function(j_self, _, _)
-      local output = table.concat(j_self:result(), "\n")
-      local stderr = table.concat(j_self:stderr_result(), "\n")
-      if stderr ~= "" then
-        vim.api.nvim_err_writeln("Error executing Python code: " .. stderr)
-      end
-
-      callback(output, stderr)
-    end),
-  }):start()
+  Job
+    :new({
+      command = executable,
+      args = { "-c", code },
+      on_exit = vim.schedule_wrap(function(j_self, _, exit_code)
+        local output = table.concat(j_self:result(), "\n")
+        local stderr = table.concat(j_self:stderr_result(), "\n")
+        if exit_code ~= 0 then
+          vim.api.nvim_err_writeln(
+            "Error executing Python code: "
+              .. (stderr ~= "" and stderr or output)
+          )
+        else
+          callback(output, stderr)
+        end
+      end),
+    })
+    :start()
 end
 
-local code_block_query = [[
-  (fenced_code_block
-    (info_string (language) @language)
-    (code_fence_content) @code_block)
-]]
+local code_block_queries = {
+  markdown = [[
+    (fenced_code_block
+      (info_string (language) @language)
+      (code_fence_content) @code_block)
+  ]],
+  octo = [[
+    (fenced_code_block
+      (info_string (language) @language)
+      (code_fence_content) @code_block)
+  ]],
+}
 
 local get_code_in_code_block = function()
-  local query = vim.treesitter.query.parse("markdown", code_block_query)
+  local filetype = vim.bo.filetype
+  local query_str = code_block_queries[filetype]
+
+  local ok, query = pcall(vim.treesitter.query.parse, filetype, query_str)
+  if not ok or not query then
+    ok, query =
+      pcall(vim.treesitter.query.parse, "markdown", code_block_queries.markdown)
+    if not ok or not query then
+      return { code = nil, language = nil, range = nil }
+    end
+    filetype = "markdown"
+  end
+
   local bufnr = vim.api.nvim_get_current_buf()
-  local root = vim.treesitter.get_parser(bufnr, "markdown"):parse()[1]:root()
+  local parser = vim.treesitter.get_parser(bufnr, filetype)
+  if not parser then
+    return { code = nil, language = nil, range = nil }
+  end
+
+  local root = parser:parse()[1]:root()
   local cursor_pos = vim.api.nvim_win_get_cursor(0)
-  local cursor_row = cursor_pos[1] - 1 -- 0-indexed
+  local cursor_row = cursor_pos[1] - 1
 
   local code_block_node = nil
   local language_node = nil
 
-  for id, node, metadata in query:iter_captures(root, bufnr, 0, -1) do
+  for id, node, _ in query:iter_captures(root, bufnr, 0, -1) do
     local name = query.captures[id]
     if name == "language" then
       language_node = node
@@ -91,16 +118,13 @@ end
 local highlight_range = function(range, timeout)
   timeout = timeout or 250
 
-  -- Highlight the code block
   local bufnr = vim.api.nvim_get_current_buf()
   local ns_id = vim.api.nvim_create_namespace "execute_code_highlight"
 
-  -- Add highlight to each line in the range
   for i = range.start, range.stop do
     vim.api.nvim_buf_add_highlight(bufnr, ns_id, "Search", i, 0, -1)
   end
 
-  -- Clear highlight after timeout ms
   vim.defer_fn(function()
     vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
   end, timeout)
@@ -135,26 +159,75 @@ M.copy_output_to_clipboard = function()
     )
   end
 
-  if
-    language == "bash"
-    or language == "sh"
-    or language == "zsh"
-    or language == "terminal"
-  then
+  if language == "bash" or language == "sh" or language == "terminal" then
     Job:new({
       command = "bash",
       args = { "-c", code },
-      on_exit = vim.schedule_wrap(function(j_self, _, _)
+      on_exit = vim.schedule_wrap(function(j_self, _, exit_code)
         local output = table.concat(j_self:result(), "\n")
         local stderr = table.concat(j_self:stderr_result(), "\n")
-        if stderr ~= "" then
-          vim.api.nvim_err_writeln("Error executing shell command: " .. stderr)
+        if exit_code ~= 0 then
+          vim.api.nvim_err_writeln(
+            "Error executing shell command: "
+              .. (stderr ~= "" and stderr or output)
+          )
+        else
+          handle_output(output)
         end
-        handle_output(output)
+      end),
+    }):start()
+  elseif language == "zsh" then
+    Job:new({
+      command = "zsh",
+      args = { "-c", code },
+      on_exit = vim.schedule_wrap(function(j_self, _, exit_code)
+        local output = table.concat(j_self:result(), "\n")
+        local stderr = table.concat(j_self:stderr_result(), "\n")
+        if exit_code ~= 0 then
+          vim.api.nvim_err_writeln(
+            "Error executing zsh command: "
+              .. (stderr ~= "" and stderr or output)
+          )
+        else
+          handle_output(output)
+        end
       end),
     }):start()
   elseif language == "python" or language == "py" then
     M.async(code, handle_output)
+  elseif language == "lua" then
+    local nvim_path = vim.fn.exepath "nvim"
+    if nvim_path == "" then
+      vim.notify(
+        "Could not find nvim executable",
+        vim.log.levels.ERROR,
+        { title = "Execute Code" }
+      )
+      return
+    end
+
+    local config_path = vim.fn.stdpath "config"
+    local tmpfile = vim.fn.tempname() .. ".lua"
+    vim.fn.writefile(vim.split(code, "\n"), tmpfile)
+
+    Job:new({
+      command = nvim_path,
+      args = { "-u", config_path .. "/init.lua", "-l", tmpfile },
+      on_exit = vim.schedule_wrap(function(j_self, _, exit_code)
+        vim.fn.delete(tmpfile)
+        local output = table.concat(j_self:result(), "\n")
+        local stderr = table.concat(j_self:stderr_result(), "\n")
+        if exit_code ~= 0 then
+          vim.api.nvim_err_writeln(
+            "Error executing Lua: " .. (stderr ~= "" and stderr or output)
+          )
+        elseif stderr ~= "" then
+          handle_output(stderr)
+        else
+          handle_output(output)
+        end
+      end),
+    }):start()
   else
     vim.notify("Unsupported language: " .. language, vim.log.levels.WARN, {
       title = "Execute Code",
